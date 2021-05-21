a1a6a081b11e2a072018b90ae03caec9e814495c
Illya King <illyaking1978@gmail.com>
2021-05-20 18:00:06 -0700

    Update index.php
    Modified Header

--- index.php
+++ index.php
@@ -11,8 +11,9 @@ include('includes/constants.php');
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mobile Commenter 2.0</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
